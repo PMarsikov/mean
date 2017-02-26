12eb2774ba04d5545deb2d10b2c4988a018e792d
pmarsikov <pmarsikov@gmail.com>
2017-02-26 18:14:42 +0300

    first xdd

--- src/server/db/db-config.js
+++ src/server/db/db-config.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var mongo = process.env.VCAP_SERVICES;
 var port = process.env.PORT || 3030;
-var conn_str = "";
+var dburl = "";
 // if bluemix service is defined
 if (mongo) {
   var env = JSON.parse(mongo);
@@ -10,21 +10,21 @@ if (mongo) {
 // in order to get url information
     mongo = env["mongodb"][0]["credentials"];
     if (mongo.url) {
-      conn_str = mongo.url;
+      dburl = mongo.url;
     } else {
       console.log("No mongo found");
     }
 // if no mongodb service bound to app
 // set default local server path
   } else {
-    conn_str = "mongodb://localhost:27017";
+    dburl = "mongodb://localhost:27017";
   }
 // if no services found
 // use local server path
 } else {
-  conn_str = "mongodb://localhost:27017";
+  dburl = "mongodb://localhost:27017";
 }
-mongoose.connect(conn_str);
+mongoose.connect(dburl);
 
 
 // CONNECTION EVENTS
