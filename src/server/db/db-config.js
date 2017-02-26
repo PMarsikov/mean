2e69d301132e433746ec24b098d6bddef0bb2372
pmarsikov <pmarsikov@gmail.com>
2017-02-27 00:34:09 +0300

    first xddkkkd 209d3

--- src/server/db/db-config.js
+++ src/server/db/db-config.js
@@ -1,32 +1,9 @@
 var mongoose = require('mongoose');
-var mongo = process.env.VCAP_SERVICES;
-var port = process.env.PORT || 3030;
-var dburl = "";
-// if bluemix service is defined
-if (mongo) {
-  var env = JSON.parse(mongo);
-  if (env["mongodb"]) {
-// get predefined credentials
-// in order to get url information
-    mongo = env["mongodb"][0]["credentials"];
-    if (mongo.url) {
-      dburl = mongo.url;
-    } else {
-      console.log("No mongo found");
-    }
-// if no mongodb service bound to app
-// set default local server path
-  } else {
-    dburl = "mongodb://localhost:27017";
-  }
-// if no services found
-// use local server path
-} else {
-  dburl = "mongodb://localhost:27017";
-}
+var dburl = 'mongodb://localhost:27017/todoapp';
+
 mongoose.connect(dburl);
 
-/* 
+
 // CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
   console.log('Mongoose connected to ' + dburl);
@@ -56,7 +33,7 @@ process.on('SIGTERM', function() {
   gracefulShutdown('App termination (SIGTERM)', function() {
     process.exit(0);
   });
-}); */
+});
 
 
 // BRING IN YOUR SCHEMAS & MODELS
