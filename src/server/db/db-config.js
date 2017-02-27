62c40573c325212cb2e1a1276b6f4853a43d0ff0
pmarsikov <pmarsikov@gmail.com>
2017-02-27 23:40:34 +0300

    first commitx x x md 009

--- src/server/db/db-config.js
+++ src/server/db/db-config.js
@@ -1,7 +1,38 @@
 var mongoose = require('mongoose');
 var dburl = 'mongodb://localhost:27017/todoapp';
 var dburl = 'mongodb://sashas:sashas@ds017185.mlab.com:17185/mytasklist';//'mongodb://localhost:27017/todoapp';
+// connection configuration
+var mongo = process.env.VCAP_SERVICES;
+var port = process.env.PORT || 3030;
+var conn_str = "";
+// if bluemix service is defined
+if (mongo) {
+  var env = JSON.parse(mongo);
+  if (env["mongodb"]) {
+// get predefined credentials
+// in order to get url information
+    mongo = env["mongodb"][0]["credentials"];
+    if (mongo.url) {
+      conn_str = mongo.url;
+    } else {
+      console.log("No mongo found");
+    }
+// if no mongodb service bound to app
+// set default local server path
+  } else {
+    //conn_str = "mongodb://localhost:27017";
+    conn_str = 'mongodb://sashas:sashas@ds017185.mlab.com:17185/mytasklist';
+  }
+// if no services found
+// use local server path
+} else {
+//  conn_str = "mongodb://localhost:27017";
+    conn_str = 'mongodb://sashas:sashas@ds017185.mlab.com:17185/mytasklist';
+}
+//mongoose.connect(conn_str);
 
+
+dburl = conn_str;
 mongoose.connect(dburl);
 
 
